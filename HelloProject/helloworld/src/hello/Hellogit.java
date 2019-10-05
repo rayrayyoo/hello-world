b790b5639e5dbee105c9bb8ef9ff685f1bf68ea9
Rei Aoki <raoki@iastate.edu>
2019-10-05 15:56:06 -0500

    Add lines in hellogit

--- HelloProject/helloworld/src/hello/Hellogit.java
+++ HelloProject/helloworld/src/hello/Hellogit.java
@@ -3,5 +3,9 @@ package hello;
 public class Hellogit {
 	public void hellogit() {
 		System.out.println("Please git! This is just practice!");
+		System.out.println("==================================");
+		System.out.println("==================================");
+		System.out.println("==================================");
+		System.out.println("**********************************");
 	}
 }
